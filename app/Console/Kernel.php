f3b533cc2fa128352b10783265ae1211c2eaa7a6
Ryan Coble <ryan.m.coble@gmail.com>
2023-01-21 16:14:43 -0500

    Add metrics generate command.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         RegisterServiceCommand::class,
         UnregisterServiceCommand::class,
+        GenerateMetricsCommand::class,
         // MessageBusConsumerCommand::class
     ];
 
